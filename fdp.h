33c8f01e827ea88df83a9deb3882f049dafc9f1f
Guille-Ux <guillermoleiratemes@protonmail.com>
2026-05-03 11:17:25 +0000

    updating fdp

--- fdp.h
+++ fdp.h
@@ -1,6 +1,8 @@
 #ifndef FDP_H
 #define FDP_H
 
+#include <stdint.h>
+
 #define EXTEND_VAL 5
 
 typedef struct {
@@ -22,5 +24,12 @@ fdp_t fdp_order_inc(fdp_t num);
 fdp_t fdp_extend(fdp_t num, uint32_t times);
 
 uint32_t powd(uint32_t n);
+
+static uint32_t shrink(uint64_t n) {
+	while (n > 6710886) {
+		n /= 10;
+	}
+	return (uint32_t)n;
+}
 
 #endif
